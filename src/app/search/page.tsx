cee1fe6de09f75c4a4d3d97a46727b7276852d28
Boaz Amakye Adjei <boazamakyeadjei@gmail.com>
2025-07-10 09:48:12 -1200

    sixth

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
 // import { Card } from "../catalog/page";
 
 export default function SearchQuery() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("q");
+  // const searchParams = useSearchParams();
+  // const query = searchParams.get("q");
 
   return (
     <main className="p-10">
-      <h2 className="text-2xl font-bold mb-5">Search result for {query} ...</h2>
+      {/* <h2 className="text-2xl font-bold mb-5">Search result for {query} ...</h2> */}
 
       <section className="grid grid-cols-4 gap-4">
         {/* <Card />
